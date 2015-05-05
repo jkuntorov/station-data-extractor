8b91155e81d1ab527d28a2b2204407c51447b674
Julian Kuntorov <julian.kuntorov@gmail.com>
2015-05-05 16:40:28 +0100

    Made a little program which spits out a text list of the stations in the journey and location extractors to make comparison easier.

---
+++ compare.rb
@@ -0,0 +1,32 @@
+require 'json'
+
+journeys_json = JSON.parse(File.read("export/stations.json"))
+locations_json = JSON.parse(File.read("export/loc.json"))
+
+journeys = []
+locations = []
+
+puts "Parsing journeys..."
+journeys_json.each do |j|
+	journeys << j["name"]
+end
+
+puts "Parsing locations..."
+locations_json.each do |j|
+	locations <<  j[0]
+end
+
+puts "Sorting arrays..."
+journeys.sort!
+locations.sort!
+
+puts "Starting to write in a file..."
+File.open('export/compare.txt', 'w') do |f|
+  f.puts "JOURNEYS"
+  f.puts journeys
+  f.puts "\n"
+  f.puts "LOCATIONS"
+  f.puts locations
+end
+
+puts "Done."
